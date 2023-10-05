ceb6f77d08d43955d0d8eccbe366d0cd4a8b555b
ahmed <ahmed.ebrahim.5115@gmail.com>
2023-10-06 01:36:37 +0300

    create islamy tab

--- lib/tabs/radio_tab.dart
+++ lib/tabs/radio_tab.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:islamyapp/MyThemeData.dart';
 
 class RadioTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.yellow,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image(image: AssetImage('assets/images/radio_image.png')),
+        SizedBox(
+          height: 50,
+        ),
+        Text('إذاعة القرآن الكريم'),
+        SizedBox(
+          height: 50,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            ImageIcon(
+              AssetImage('assets/images/Icon metro-next.png'),
+              color: MyThemeData.primaryColor,
+              size: 50,
+            ),
+            ImageIcon(
+              AssetImage('assets/images/Icon awesome-play.png'),
+              color: MyThemeData.primaryColor,
+              size: 50,
+            ),
+            ImageIcon(
+              AssetImage('assets/images/Icon metro-back.png'),
+              color: MyThemeData.primaryColor,
+              size: 50,
+            ),
+          ],
+        )
+      ],
     );
   }
 }
